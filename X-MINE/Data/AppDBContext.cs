9fce6da3e40c873447f29bce4f65f46daaa248c3
lulusbagos <lulusbagoshermawan@gmail.com>
2024-08-06 16:03:24 +0800

    update model sama data

---
+++ X-MINE/Data/AppDBContext.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Mvc;
+
+namespace X_MINE.Data
+{
+    public class AppDBContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public AppDBContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(Configuration.GetConnectionString("WebApiDatabase"));
+        }
+
+        public DbSet<Models.vw_t_user_kategori> VwTUserKategoris { get; set; }
+        public DbSet<Models.tbl_r_menu> tbl_r_menu { get; set; }
+        public DbSet<Models.tbl_r_kategori_user> tbl_r_kategori_user { get; set; }
+        public DbSet<Models.tbl_r_dept> tbl_r_dept { get; set; }
+        public DbSet<Models.tbl_m_user_login> tbl_m_user_login { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+
+            modelBuilder.Entity<Models.vw_t_user_kategori>()
+                .HasNoKey()
+                .ToView("vw_t_user_kategori");
+
+            modelBuilder.Entity<Models.tbl_r_kategori_user>(entity =>
+            {
+                entity.HasKey(e => e.id);
+            });
+
+            modelBuilder.Entity<Models.tbl_r_dept>(entity =>
+            {
+                entity.HasKey(e => e.id);
+            });
+            modelBuilder.Entity<Models.tbl_r_menu>(entity =>
+            {
+                entity.HasKey(e => e.id);
+            });
+
+            modelBuilder.Entity<Models.tbl_m_user_login>(entity =>
+            {
+                entity.HasKey(e => e.id);
+            });
+
+
+        }
+    }
+}
---
+++ X-MINE/Models/tbl_r_menu.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace X_MINE.Models
+{
+    [Table("tbl_r_menu")]
+    public class tbl_r_menu
+    {
+        [Key]
+        [Column("id")]
+        public int id { get; set; }
+
+        [Column("kategori_user_id")]
+        public string? kategori_user_id { get; set; }
+
+        [Column("title")]
+        public string? title { get; set; }
+
+        [Column("type")]
+        public string? type { get; set; }
+
+        [Column("link_controller")]
+        public string? link_controller { get; set; }
+
+        [Column("link_function")]
+        public string? link_function { get; set; }
+
+        [Column("hidden")]
+        public string? hidden { get; set; }
+
+        [Column("new_tab")]
+        public string? new_tab { get; set; }
+
+        [Column("insert_by")]
+        public string? insert_by { get; set; }
+
+        [Column("ip")]
+        public string? ip { get; set; }
+
+        [Column("created_at")]
+        public DateTime? created_at { get; set; }
+
+        [Column("updated_at")]
+        public DateTime? updated_at { get; set; }
+    }
+}
